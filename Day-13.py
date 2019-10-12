3fb377c6e514f06aef7c6968543aebe203b90e3e
MasumTech <masumrezadiu@gmail.com>
2019-10-12 22:57:29 +0600

    Solution of Day 16: Exceptions - String to Integer in Python

--- Day-13.py
+++ Day-13.py
@@ -1,29 +1,19 @@
-from abc import ABCMeta, abstractmethod
-class Book:
-    __metaclass__ = ABCMeta
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author
-    @abstractmethod
-    def display(): pass
-class MyBook(Book):
-    def __init__(self,title, author, price):
-        self.title=title
-        self.author=author
-        self.price = price
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
 
-    def display(self):
-        print('Title: '+self.title)
-        print('Author: '+self.author)
-        print('Price: '+str(self.price))
+    # Add your code here
+    def computeDifference(self):
+        mx = max(self.__elements)
+        mn = min(self.__elements)
+        self.maximumDifference = mx - mn
+    # End of Difference class
 
 
-#Write MyBook class
+_ = raw_input()
+a = [int(e) for e in raw_input().split(' ')]
 
+d = Difference(a)
+d.computeDifference()
 
-
-title=raw_input()
-author=raw_input()
-price=int(raw_input())
-new_novel=MyBook(title,author,price)
-new_novel.display()
+print d.maximumDifference
